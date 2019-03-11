96a38303afac02ad09e006083ac3cc1d428279d4
Vladimir Fomene <vladimirfomene@gmail.com>
2019-03-11 18:04:45 +0100

    create and expose a meal resource and an email resource

---
+++ routes/api/email.js
@@ -0,0 +1,33 @@
+var Email = require('keystone-email');
+
+/***
+Send an email
+===============
+**/
+exports.create = (req, res) => {
+
+	new Email('order-email.pug', {
+		transport: 'mailgun',
+	}).send({
+		recipient: {
+					firstName: 'Vladimir',
+					lastName: 'Fomene',
+			},
+		orders: req.body.orders,
+		}, {
+		apiKey: process.env.MAILGUN_API_KEY,
+		domain: process.env.MAILGUN_DOMAIN,
+		to: 'vladimirfomene@gmail.com',
+		from: {
+			name: 'Food Delivery App',
+			email: 'orders@food-delivery-app.com',
+		},
+		subject: 'Urgent Food Order',
+	}, function (err, result) {
+		if (err) {
+			console.error('🤕 Mailgun test failed with error:\n', err);
+		} else {
+			console.log('📬 Successfully sent Mailgun test with result:\n', result);
+		}
+	});
+};
---
+++ routes/api/meal.js
@@ -0,0 +1,84 @@
+var keystone = require('keystone');
+var Meal = keystone.list('Meal');
+
+
+/***
+List Meals
+===============
+**/
+exports.all = (req, res) => {
+	Meal.model.find((err, meals) => {
+		if(err) return res.json({ err: err});
+
+		res.json(meals);
+	});
+};
+
+/***
+Get a meal
+===============
+**/
+exports.get = (req, res) => {
+	Meal.model.findById(req.params.id).exec((err, meal) => {
+		if(err) return res.json(err);
+		if(!meal) return res.status(404).send('No meal with that ID');
+		res.json({meal: meal});
+	});
+};
+
+/***
+Create a meal
+===============
+**/
+exports.create = (req, res) => {
+	if(typeof req.body.title !== 'string')
+		return res.status(400).send("Title is invalid");
+
+	if(typeof req.body.description !== 'string')
+		return res.status(400).send("Description is invalid");
+
+	if(typeof req.body.image_url !== 'string')
+			return res.status(400).send("Image url is invalid");
+
+	var newMeal = new Meal.model({
+			title: req.body.title,
+			description: req.body.description,
+			image_url: req.body.image_url,
+	});
+
+	newMeal.save((err, meal) => {
+		if(err) return res.json(err);
+
+		return res.status(201).json(meal);
+	});
+};
+
+/***
+Delete a meal
+===============
+**/
+exports.delete = (req, res) => {
+	Meal.model.findById(req.params.id).remove((err) => {
+		if(err) return res.json(err);
+		res.status(200).json({msg: 'Meal deleted successfull'});
+	});
+};
+
+/***
+Update a meal
+===============
+**/
+exports.update = (req, res) => {
+	Meal.model.findById(req.params.id).exec((err, meal) => {
+		if(err) return res.json(err);
+		if(!meal) return res.status(404).send('No meal with that ID');
+
+		var data = (req.method == 'PUT') ? req.body : req.query;
+
+		meal.getUpdateHandler(req).process(data, (err) => {
+			if(err) return res.json(err);
+			res.json(meal);
+		})
+
+	});
+};
