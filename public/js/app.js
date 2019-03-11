d04e059d90435e422d2cfd8b5ea28297a16536a4
Vladimir Fomene <vladimirfomene@gmail.com>
2019-03-11 18:59:17 +0100

    add auth0 Signup/Login and control display based on authentication

---
+++ public/js/app.js
@@ -0,0 +1,168 @@
+const basket = {};
+const checkoutBtn = $('#btn-checkout');
+
+function addToBasket(meal){
+	if(meal in basket){
+		basket[meal] += 1
+		console.log(basket[meal]);
+	}else{
+		basket[meal] = 1;
+		console.log(basket[meal]);
+	}
+
+	basket['count'] = 'count' in basket? basket['count'] + 1 : 1;
+
+
+	checkoutBtn.html("Checkout (" + basket['count'] + ")");
+}
+
+$('document').ready(function() {
+	const apiUrl = 'http://localhost:3000/api';
+
+	// create an Auth0 client
+	const webAuth = new auth0.WebAuth({
+		domain: 'fomene.auth0.com',
+		clientID: 'TW8hW6p0Ss1gboLlGsS7y93c30jY64JZ',
+		redirectUri: location.href,
+		audience: 'http://localhost:3000/api',
+		responseType: 'token id_token',
+		scope: 'openid profile read:messages',
+		leeway: 60
+	});
+
+	// select HTML elements to be manipulated
+	const loginBtn = $('#btn-login');
+	const logoutBtn = $('#btn-logout');
+	const orderBtn = $('#btn-order');
+	const promoView = $('.promo-message');
+	const bodyView = $('body');
+	const mainContainer = $('.container');
+
+	// add listeners to buttons
+	loginBtn.click(() => webAuth.authorize());
+	logoutBtn.click(logout);
+	orderBtn.click(() => placeOrder());
+	checkoutBtn.click(addOrdersToReceipt);
+
+
+
+	let accessToken = null;
+	let userProfile = null;
+
+	handleAuthentication();
+	controlDisplay();
+
+	// function definitions
+	function logout() {
+		// Remove tokens and expiry time from browser
+		accessToken = null;
+		controlDisplay();
+	}
+
+	function isAuthenticated() {
+		return accessToken != null;
+	}
+
+	function handleAuthentication() {
+		webAuth.parseHash(function(err, authResult) {
+			if (authResult && authResult.accessToken) {
+				window.location.hash = '';
+				accessToken = authResult.accessToken;
+				userProfile = authResult.idTokenPayload;
+				//call api for meals if isAuthenticated
+				getMenu();
+				loginBtn.css('display', 'none');
+				logoutBtn.css('display', 'inline-block');
+
+			} else if (err) {
+				console.log(err);
+				alert(
+					'Error: ' + err.error + '. Check the console for further details.'
+				);
+			}
+			controlDisplay();
+		});
+	}
+
+	function setupUrlAndReqheaders(endpoint, secured){
+		const url = apiUrl + endpoint;
+
+		let headers;
+		if (secured && accessToken) {
+			headers = { Authorization: 'Bearer ' + accessToken };
+		}
+
+		return {url, headers};
+	}
+
+	function getMenu() {
+		const {url, headers} = setupUrlAndReqheaders('/meals', true)
+		$.ajax({
+			type: 'GET',
+			url: url,
+			headers: headers,
+			success: (data) => {
+				renderMeals(data);
+			},
+			error: (data) => {console.log(JSON.parse(data.responseText))}
+		});
+	}
+
+	function placeOrder(){
+		const {url, headers} = setupUrlAndReqheaders('/emails', true)
+
+		$.ajax({
+			type: 'POST',
+			url: url,
+			data: {
+				orders: basket
+			},
+			headers: headers,
+			success: (data) => {
+				console.log("Your email was sent successfully");
+			},
+			error: (data) => {console.log(data)}
+		});
+	}
+
+
+	function renderMeals(meals){
+		var mealContainer = $('#menu-view');
+		for(key in meals){
+			mealContainer.append('<div class="card col-md-4" style="width: 30rem;"><img src="'+
+				meals[key].image_url.url + '" class="card-img-top" alt="'+ meals[key].title +
+				'"><div class="card-body"><h5 class="card-title">'
+				+ meals[key].title + '</h5><p class="card-text">' +
+				meals[key].description + '</p><button onclick="addToBasket(\'' + meals[key].title + '\')" class="btn btn-primary">Order</button></div></div>');
+		}
+	}
+
+
+	function controlDisplay() {
+		if (isAuthenticated()) {
+			loginBtn.css('display', 'none');
+			logoutBtn.css('display', 'inline-block');
+			checkoutBtn.css('display', 'inline-block');
+			checkoutBtn.css('margin-left', 'auto');
+			mainContainer.css('display', 'block');
+			promoView.css('display', 'none');
+			bodyView.css('background-image', 'none');
+		} else {
+			loginBtn.css('display', 'inline-block');
+			logoutBtn.css('display', 'none');
+			checkoutBtn.css('display', 'none');
+			mainContainer.css('display', 'none');
+			promoView.css('display', 'block');
+			bodyView.css('background-image', 'url("/images/bg-home.jpg")');
+		}
+	}
+
+	function addOrdersToReceipt(){
+		var receiptBody = $('#receipt-body');
+		receiptBody.empty();
+		for(meal in basket){
+			if(meal != 'count') receiptBody.append('<tr><td>' + meal + '</td><td>' + basket[meal] + '</td></tr>');
+		}
+	}
+
+});
